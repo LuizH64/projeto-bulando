4567b612f228352cdbb5728e4b3d54b32f55869a
Luiz Rodrigues <luizhenriquealvesrodrigues64@gmail.com>
2022-06-20 21:19:49 -0300

    first-commit

---
+++ src/screens/Login/LoginScreen.js
@@ -0,0 +1,12 @@
+// Dependencias
+import React from "react";
+
+const LoginScreen = () => {
+    return (
+        <div>
+            LoginScreen
+        </div>
+    )
+}
+
+export default LoginScreen;
